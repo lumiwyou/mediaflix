118c46e03be13449568396d5102feeb507109196
Lumi Hyväri <lumi.hyvari@gmail.com>
2026-05-09 03:08:47 +0200

    Switched from js to typescript

--- src/routes/routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-//import bookController from "./root/root.controller.js";
-
-const api = Router()
-    // Route controllers here
-    //.use(rootController)
-
-export default Router().use("/api", api);
