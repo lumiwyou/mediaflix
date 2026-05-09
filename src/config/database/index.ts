5786fceb2f728a9ec3ddf70486b5ff60e408945a
Lumi <lumi.hyvari@gmail.com>
2026-05-09 23:06:32 +0200

    Added verbosity to database module

--- src/config/database/index.ts
+++ src/config/database/index.ts
@@ -7,7 +7,16 @@ dotenv.config({ path: config.environment_path })
 export const database = mysql.createConnection({
   host: process.env.PGHOST,
   user: process.env.PGUSER,
-  password: process.env.PGPASSWORD
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  connectTimeout: 10000
 });
+
+console.debug(`Database connection configuration:
+  host: ${process.env.PGHOST}
+  user: ${process.env.PGUSER}
+  password: ${process.env.PGPASSWORD?.replaceAll(/./g, '*')}
+  database: ${process.env.PGDATABASE}
+  `)
 
 export default database
