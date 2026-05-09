f99f3e251d588ca314cd8a15519caf64f1ec7923
Lumi <lumi.hyvari@gmail.com>
2026-05-09 23:07:56 +0200

    Reflecting export type change

--- src/modules/v1/user/service/user.service.ts
+++ src/modules/v1/user/service/user.service.ts
@@ -1,5 +1,5 @@
 import connection from "../../../../config/database/index.ts"
-import User from "../model/user.model.ts"
+import { type User } from "../model/user.model.ts"
 
 export async function getById(uuid: string) {
     const user = await new Promise<User>((resolve, reject) => connection.query(`
@@ -26,14 +26,13 @@ export async function createUser(user: User) {
         INSERT INTO users('uuid', 'email', 'password_hash') VALUES('${user.uuid}', '${user.email}', '${user.password_hash}')
         `)
     const res = await getById(user.uuid)
-    if(res.email == null) {
-        return false
-    }
+    if(res.email == null) return false
     return true
 }
 
 export async function deleteUser(modify_user: User) {
     // Schedule for deletion
+    // TODO: Implement routine deletion procedure
     connection.query(`
         UPDATE users
         SET users.deletion = ${modify_user.deletion}, users.locked = ${modify_user.locked}
@@ -41,10 +40,20 @@ export async function deleteUser(modify_user: User) {
         `)
 
     const res = await getById(modify_user.uuid)
-    if(res.deletion != true || res.locked != true) {
-        return false
-    }
-    return true;
+    if(res.deletion != true || res.locked != true) return false
+    return true
 }
 
-export default { getById, createUser, deleteUser }
+export async function updateUser(modify_user: User) {
+    connection.query(`
+        UPDATE users
+        SET users.email = ${modify_user.email}, users.password_hash = ${modify_user.password_hash}
+        WHERE users.id == ${modify_user.uuid}
+        `)
+    
+    const res = await getById(modify_user.uuid)
+    if(res.email != modify_user.email || res.password_hash != modify_user.password_hash) return false
+    return true
+}
+
+export default { getById, createUser, deleteUser, updateUser }
