f8d3a4c6cfd04b6615e50baa1dfc3904cd469b0c
Lumi <lumi.hyvari@gmail.com>
2026-05-09 23:08:15 +0200

    Added `userController.updateUser` to `user.routes`

--- src/modules/v1/user/routes/user.routes.ts
+++ src/modules/v1/user/routes/user.routes.ts
@@ -6,5 +6,6 @@ export const router = Router()
 router.get("/:id", userController.getById)
 router.post("/", userController.createUser)
 router.delete("/", userController.deleteUser)
+router.put("/", userController.updateUser)
 
 export default router
