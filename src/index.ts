1ad30e5f4909b7045f839529102c4e438ed11b3d
Lumi <lumi.hyvari@gmail.com>
2026-05-10 03:51:36 +0200

    Added `printRegisteredRoutes` utility function

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,25 @@
 import express from "express"
 import morgan from "morgan"
 import config from "../settings.json" with { type: 'json' }
-import api from "./modules/v1/index.ts"
+import apiRouter_v1 from "./modules/v1/index.ts"
 import database from "./config/database/index.ts"
 import { exit } from "node:process"
+import printRegisteredRoutes from "./utils/routes.ts"
 
-const app = express();
+const app = express()
 
 // App configuration
-app.use(api);
-app.use(express.static("public"));
+app.get("/api", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: `
+        Available endpoints: /api/v1/users: GET,POST,PUT,DELETE
+        `
+    })
+})
+
+app.use("/api/v1", apiRouter_v1)
+app.use(express.static("public"))
 
 try {
     console.info("Connecting to database ...")
@@ -27,11 +37,11 @@ try {
 }
 
 if(config.logging.enabled) {
-    app.use(morgan(config.logging.format));
+    app.use(morgan(config.logging.format))
 }
 
 app.listen(config.network.port, config.network.host, () => {
-    console.info(`Running up service @ http://${config.network.host}:${config.network.port}`);
+    console.info(`Running up service @ http://${config.network.host}:${config.network.port}\n`)
 })
 
-// database.close()
+printRegisteredRoutes(app.router.stack)
