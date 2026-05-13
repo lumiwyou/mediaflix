36bb9d91f7803c6aba284e7b746940a929d19c79
Lumi <lumi.hyvari@gmail.com>
2026-05-13 23:21:26 +0200

    Added library module

--- src/modules/v1/index.ts
+++ src/modules/v1/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express"
 
 /* Import the modules by path "./<module-directory>/index.ts" */
-import usersRouter from "./user/index.ts"
+import usersRouter from "./users/index.ts"
+import libraryRouter from "./library/routes/library.routes.ts"
 
 const apiRouter_v1: Router = Router()
     .use(usersRouter)
+    .use(libraryRouter)
 
 export default apiRouter_v1
